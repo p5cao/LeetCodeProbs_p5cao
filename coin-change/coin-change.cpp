a7794792454ad95d5981beac1e2327ebf75f87c9
p5cao <33706047+p5cao@users.noreply.github.com>
2021-10-06 21:06:54 -0700

    Time: 104 ms (39.51%), Space: 18.3 MB (18.86%) - LeetHub

---
+++ coin-change/coin-change.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        double inf = std::numeric_limits<double>::infinity();
+        vector<double> DP(amount+1, inf);
+        DP[0] = 0;
+        for (int a = 1; a< amount+1; a++){
+            for (int i = 0; i< coins.size(); i++){
+                if (a >= coins[i]){
+                    DP[a] = min(DP[a], 1 + DP[a-coins[i]]);
+                }
+            }
+        }
+        
+        if (DP[amount] != inf){
+            return DP[amount];
+        }else{
+            return -1;
+        }
+    }
+};
+                 
